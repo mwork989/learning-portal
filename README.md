e623bb6e6c78ea0487bf8d5d0c285bcdb096979e
unknown <maheshwork989@gmail.com>
2023-04-30 09:16:33 +0530

    adding updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# learning-portal
-learning-portal project
+node and npm commands
+
+
+npm init -- creates the package.json file which has reference for tools depdencies
+npm install <packagename>  ( to be identified fom npmjs.com
---
+++ js/app/register-script.js
@@ -0,0 +1,21 @@
+
+
+function submit(event){
+    console.log(event)
+    var email = document.getElementById('inputEmail4').value;
+    var password = document.getElementById('password').value;
+    var designation = document.getElementById('designation').value;
+    var inputAddress = document.getElementById('inputAddress').value;
+    var inputAddress2 = document.getElementById('inputAddress2').value;
+    
+    if(email && password && designation && inputAddress && inputAddress2){
+        var user= {};
+        user.email = email;
+        user.password = password;
+        user.designation = designation;
+        user.inputAddress = inputAddress;
+        user.inputAddress2 = inputAddress2;
+    }
+    alert(user);
+    return false;
+}
